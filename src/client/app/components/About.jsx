411aca8d9c33e6f127054d96ac031f22757b3f08
jeffleu <jeffreycleu@gmail.com>
2017-10-07 18:24:05 -0700

    Added Redux to boilerplate

--- src/client/app/components/About.jsx
+++ src/client/app/components/About.jsx
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 class About extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-
-    };
   }
 
   render() {
--- src/client/app/components/Home.jsx
+++ src/client/app/components/Home.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement, incrementAmount } from '../actions/actions';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-
-    };
   }
 
   render() {
     return (
       <div>
         <p>Home</p>
+
+        <div>
+          <form onSubmit={ this.props.handleIncrementAmount }>
+            <input type="number" placeholder="Enter an amount to add."/>
+            <button type="submit">Submit</button>
+          </form>
+
+          Clicked: <span id="value">{ this.props.counter }</span>
+          <button onClick={ this.props.handleIncrement }>+</button>
+          <button onClick={ this.props.handleDecrement }>-</button>
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({counter: state.counter});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleIncrement: () => {
+      dispatch(increment());
+    },
+    handleDecrement: () => {
+      dispatch(decrement());
+    },
+    handleIncrementAmount: (event) => {
+      event.preventDefault();
+      dispatch(incrementAmount(5));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
